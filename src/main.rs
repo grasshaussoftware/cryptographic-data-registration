d69bc532d44da3199061b57a1bf491429f18e841
grasshaussoftware <deusopus@duck.com>
2023-11-08 22:36:14 -0800

    security update and other bug fixes.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use std::time::{ Duration };
 use tokio; // for async runtime
 use rand_core::{ SeedableRng };
 use chrono::{Utc, DateTime};
+use sha2::{Sha256, Digest};
+
 
 // Replace this placeholder with your actual Ethereum node RPC URL and your Infura project ID
 const AVAX_RPC_URL: &str =
@@ -42,6 +44,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut maiden = String::new();
         let mut ether = String::new();
         let mut handle = String::new();
+        let mut random_input = String::new();
+
+        println!("Welcome to the Avalanche-C Global Identity Registration System.");
+        println!("Please enter your information below to register your global identity on the Avalanche-C blockchain.");
+        println!("Your information will be hashed and used to generate a unique public key and seed phrase.");
+        println!("Your seed phrase will be displayed as a QR code and printed to the thermal printer.");
+        println!("Your public key will be displayed as a QR code and printed to the thermal printer.");
+        println!("Your private key will be generated and destroyed from memory.");
+        println!("Your private key will not be saved anywhere.");
+
+        println!("Please press Enter to continue...");
+        let mut proceed = String::new();
+        io::stdin().read_line(&mut proceed).expect("Failed to read line");
 
         println!("Please enter your name:");
         io::stdin().read_line(&mut name).expect("Failed to read line");
@@ -49,23 +64,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Please enter your mother's maiden name:");
         io::stdin().read_line(&mut maiden).expect("Failed to read line");
 
-
-
         println!("Please enter your Ethereum public key:");
         io::stdin().read_line(&mut ether).expect("Failed to read line");
 
         println!("Please enter your handle on X.com:");
         io::stdin().read_line(&mut handle).expect("Failed to read line");
 
+        // Create a URL for the user's X.com profile
         let url = format!("https://x.com/{}", handle.trim());
-        
-        // Call to get the current Ethereum block hash as a seed
-        let block_hash_seed = get_current_block_hash().await.expect(
-            "Failed to get current block hash"
-        );
 
-        // Create a seeded RNG
-        let mut rng = rand_hc::Hc128Rng::from_seed(block_hash_seed);
+        println!("Please enter a random number number from 0-999:");
+        io::stdin().read_line(&mut random_input).expect("Failed to read line");
+    
+        let random_number: u64 = random_input.trim().parse().expect("Please type a number!");
+    
+        // Call to get the current block hash as a seed
+        let block_hash_seed = get_current_block_hash().await.expect("Failed to get current block hash");
+    
+        // Convert the block hash to a byte array (it's already a byte array if retrieved from the block hash)
+        let block_hash_bytes = block_hash_seed;
+    
+        // Convert the random number to a byte array
+        let random_bytes = random_number.to_be_bytes();
+    
+        // Hash the combined inputs
+        let mut hasher = Sha256::new();
+        hasher.update(&block_hash_bytes);
+        hasher.update(&random_bytes);
+        let hash_result = hasher.finalize();
+        let mut rng_seed = [0u8; 32];
+        rng_seed.copy_from_slice(&hash_result.as_slice()[0..32]);
+    
+        // Now rng_seed contains the hashed result of both the block hash and the user's random number,
+        // and can be used as a high-entropy seed for your RNG
+        let mut rng = rand_hc::Hc128Rng::from_seed(rng_seed);
 
         // Generate RSA keys using the seeded RNG
         let bits = 2048;
@@ -85,10 +117,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Print the seed phrase, time stamp, and QR code to the thermal printer
         //https://docs.rs/thermal-print/latest/thermal_print/
 
+        // Print block hash
+        println!("Your block hash seed: {:?}", block_hash_seed);
+
         // Print the mnemonic phrase
         println!("Your seed phrase (mnemonic): {}", phrase);
+        
         // Print the timestamp
         println!("{}", now);
+
         // Convert mnemonic phrase to QR code and display it
         let code = QrCode::new(phrase).unwrap();
         let image = code.render::<unicode::Dense1x2>().build();
@@ -106,6 +143,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "ether": ether.trim(),
             "x.com": url,
             "timestamp": now,
+            "block_hash": block_hash_seed,
             "public_key": public_key_pem
         });
 
